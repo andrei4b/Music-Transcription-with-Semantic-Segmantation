362469d7a9d9acac2f23015e56476fdfc34be788
andreib <andrei4.bulzan@gmail.com>
2019-05-24 15:45:28 +0300

    started adapting for MAESTRO

---
+++ MAESTRO/MAESTRO_FeatureExtraction.py
@@ -0,0 +1,135 @@
+import sys
+sys.path.append("../MusicNet")
+
+import glob, os
+import pickle
+from FeatureExtraction import Manage_Feature_Process
+from MAESTRO_ProcessLabel import ProcessLabel
+import argparse
+import numpy as np
+from tqdm import trange
+
+
+def list_wavs(path):
+    '''
+    receive a path/a list of paths, and return a list with all wavs from this location(s)
+    '''
+    if type(path) == str:
+        return glob.glob(os.path.join(path, "*.wav"))
+    
+    assert(type(path)==list)
+    files = []
+    for p in path:
+        ff = glob.glob(os.path.join(p, "*.wav"))
+        files.append(ff)
+        
+    return files
+
+def Manage_Process_Label(files, save_path, num_per_file, t_unit=0.02):
+    '''
+		- make a list with txt file paths
+		- iters = number of data files 
+		- sub-files = sublist of txts
+		- for each file from the sublist, call ProcessLabel, 
+        add the result to labels and create a pickle file for each txt sublist  
+    '''
+    files = [ff.replace(".wav", ".midi") for ff in files]
+    
+    iters = np.ceil(len(files)/num_per_file).astype('int')
+    for i in trange(iters):
+        sub_files = files[i*num_per_file : (i+1)*num_per_file]
+        
+        labels = []
+        for sf in sub_files:
+            labels.append(ProcessLabel(sf, t_unit=t_unit))
+
+        f_name = "train" if "train" in save_path else "test"
+        post   = "_{}_{}_label.pickle".format(num_per_file, i+1)
+        f_name += post
+        f_name = os.path.join(save_path, f_name)
+
+        pickle.dump(labels, open(f_name, 'wb'), pickle.HIGHEST_PROTOCOL)
+
+def main(args):
+    """
+    - use just MUS folders? Only 6 train folders?
+    - train_audios, test_audios = wav files lists
+    - call Manage_Feature_Process (from MusicNet folder) and Manage_Process_Label to generate
+    features needed for training and testing
+    """
+    train_folders = ["2017"]
+    test_folders  = []
+    train_folders = [os.path.join(args.MAESTRO_path, ff) for ff in train_folders]
+    test_folders  = [os.path.join(args.MAESTRO_path, ff) for ff in test_folders]
+    
+    
+    train_audios = list_wavs(train_folders)
+    train_audios = [item for sublist in train_audios for item in sublist] #flatten list
+    # for ff in train_audios:
+    # 	print(ff)
+
+    test_audios  = list_wavs(test_folders)
+    test_audios = [item for sublist in test_audios for item in sublist] #flatten list
+    # for ff in test_audios:
+    # 	print(ff)
+
+    if args.save_path is None:
+        args.save_path = os.path.join(args.MAESTRO_path, "./features")
+    
+    train_save_path = os.path.join(args.save_path, "train")
+    if not os.path.exists(train_save_path):
+        os.makedirs(train_save_path)
+    test_save_path = os.path.join(args.save_path, "test")
+    if not os.path.exists(test_save_path):
+        os.makedirs(test_save_path)
+        
+    train_save_name = "train_" + str(args.train_num_per_file)
+    test_save_name  = "test_" + str(args.test_num_per_file)
+    
+    
+    # Process training features
+    Manage_Feature_Process(train_audios, train_save_path, train_save_name, num_per_file=args.train_num_per_file, harmonic=not args.no_harmonic)
+    #Manage_Process_Label(train_audios, train_save_path, args.train_num_per_file)
+    
+    # Process testing features
+    #Manage_Feature_Process(test_audios, test_save_path, test_save_name, num_per_file=args.test_num_per_file, harmonic=not args.no_harmonic)
+    #Manage_Process_Label(test_audios, test_save_path, args.test_num_per_file)
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description="Program to process MAESTRO features for training and testing.")
+    parser.add_argument("--MAESTRO-path", 
+                        help="Path to your downloaded MAESTRO folder path. (default: %(default)s)",
+                        type=str, default="./")
+    parser.add_argument("--train-num-per-file", 
+                        help="Number of pieces to be stored in each output train file (default: %(default)d)",
+                        type=int, default=20)
+    parser.add_argument("--test-num-per-file", 
+                        help="Number of pieces to be stored in each output test file (default: %(default)d)",
+                        type=int, default=10)
+    parser.add_argument("--save-path",
+                        help="Path for saving the output files (default: <MAESTRO>/features)",
+                        type=str)
+    parser.add_argument("--no-harmonic",
+                        help="Wether to generate harmonic features (HCFP, 12 channels) or CFP features only(4 channels).",
+                        action="store_true")
+    args = parser.parse_args()
+    
+    
+    main(args)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ MAESTRO/MAESTRO_ProcessLabel.py
@@ -0,0 +1,89 @@
+import os
+import h5py
+import librosa
+import argparse
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+
+def ProcessLabel(gt_path, t_unit=0.02, length=None, pitch_width=352, base=88):
+    """
+        The variable 'length' should be the number of total frames.
+        ---
+        - read lines from txt file
+        - is there a serios problem with the \t in the txt files? DEBUG
+        - extract onset (float), offset (float) and midi (int)
+        - ?? start/end frame
+        - calculate pitch (between 0 and 88)
+        - add [start_frm, end_frm, pitch] to queue
+        - queue[-1][1] = the value of the last end_frm. 
+        - length = max frame number + 100
+        - label = matrix with length lines and pitch_width (352) columns, init with 0
+        - scale = 4
+        - put 1's in label on corresponding onset/offset intervals, on 4 adiacent pitch values
+        - print song length in seconds
+    """
+
+    with open(gt_path, "r") as ll_file:
+        lines = ll_file.readlines()
+
+    queue = []
+    base_note = librosa.note_to_midi("A0")
+
+    for i in range(1, len(lines)):
+        if(len(lines[i].split("\t")) != 4):
+            print("Newline")
+            continue
+        onset, offset, midi, velocity = lines[i].split("\t")
+        onset, offset, midi, velocity = float(onset), float(offset), int(midi), int(velocity[:velocity.find("\n")])
+        
+        start_frm, end_frm = round(onset/t_unit), round(offset/t_unit)
+        pitch = midi - base_note
+        
+        queue.append([start_frm, end_frm, pitch])
+    
+    if length is not None:
+        assert(length >= queue[-1][1]), "The given length cannot be shorter than the real length! Please specify a longer one."
+    else:
+        length = queue[-1][1] + 100
+    
+    
+    assert(pitch_width % base == 0)
+    label = np.zeros((length, pitch_width))
+    scale = pitch_width // base
+
+    for note in queue:
+        on, off, p = note
+        p_range = range(p*scale, (p+1)*scale)
+        label[on:off, p_range] = 1
+
+    print("Time (sec): ", length*t_unit)
+    
+    return label
+
+if __name__ == "__main__":
+    '''
+        example for testing 
+    '''
+    ff_name = "./maps/AkPnBcht/MUS/MAPS_MUS-alb_se3_AkPnBcht.txt"
+    
+    label = ProcessLabel(ff_name)
+
+    plt.imshow(label.transpose(), aspect='15', origin='lower')
+    plt.savefig("image.png", bbox_inches='tight', dpi=200)
+    plt.show()
+    
+
+
+
+
+
+
+
+
+    
+    
+    
+    
